af4588d9093ae3a11d7f2ff2e3b805814c21b1ba
James-OO7 <jamesbond03@icloud.com>
2024-09-20 13:17:49 -0400

    changed socket url to connect to backend

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://james-chat-backend.onrender.com');
 
 export const sendMessage = (message) => {
   socket.emit('chatMessage', message);
